101c3a0d0e2901e94f1bbf52810fd974d3ab303e
ksoni02 <krishna.soni@tufts.edu>
2014-03-23 00:48:41 -0400

    formatting table

--- transit/transit.js
+++ transit/transit.js
@@ -462,7 +462,7 @@ function createTLocations()
           parsePredictions(key);
           parseTable();
           curMarker['infoWindow'] = new google.maps.InfoWindow({
-            content: "<u>"+TStationsLookup[key].Station+"</u> <br> Predictions (Destination - Time) <br><br>Oak Grove   (2min 58s)<br>Forest Hills   (0min 14s)<br>Oak Grove   (5min 53s)<br>Oak Grove   (16min 45s)<br>Forest Hills   (3min 9s)<br>Forest Hills   (14min 1s)<br>Forest Hills   (18min 56s)<br>"
+            content: "<u>"+TStationsLookup[key].Station+"</u> <br> Predictions Destination - Time <br>Oak Grove - 2min 58s<br>Forest Hills - 0min 14s<br>Oak Grove - 5min 53s<br>Oak Grove - 16min 45s<br>Forest Hills - 3min 9s<br>Forest Hills - 14min 1s<br>Forest Hills - 18min 56s<br>"
           });
           
           google.maps.event.addListener(curMarker, 'click', function() {
